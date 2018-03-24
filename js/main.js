9439d810fcc0789450c786c67467347dd5948cf2
Piotr_Pietraszek <pan.pietraszek@gmail.com>
2018-03-24 17:41:38 +0100

    cos

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-window.onscroll = function() {myFunction()};
+  window.onscroll = function () { myFunction() };
 });
 
 function myFunction() {
-    var navbar = document.getElementById("navi");
+  var navbar = document.getElementById("navi");
 
-      console.log(window.pageYOffset);
+  console.log(window.pageYOffset);
   if (window.pageYOffset >= 80) {
-            navbar.style.backgroundColor = "rgba(0, 0, 0, 0.5)";}
+    navbar.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+  }
   else {
-   navbar.style.backgroundColor = "transparent";
+    navbar.style.backgroundColor = "transparent";
   }
 }
