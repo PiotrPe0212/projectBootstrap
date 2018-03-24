92c1feae972627519e37312601e05f030387c1f4
Piotr_Pietraszek <pan.pietraszek@gmail.com>
2018-03-24 10:13:42 +0100

    drobne zmiany

--- js/main.js
+++ js/main.js
@@ -5,11 +5,9 @@ window.onscroll = function() {myFunction()};
 function myFunction() {
     var navbar = document.getElementById("navi");
 
-    console.log("a");
-    console.log(window.pageYOffset);
+      console.log(window.pageYOffset);
   if (window.pageYOffset >= 80) {
-      console.log("b");
-       navbar.style.backgroundColor = "rgba(0, 0, 0, 0.5)";}
+            navbar.style.backgroundColor = "rgba(0, 0, 0, 0.5)";}
   else {
    navbar.style.backgroundColor = "transparent";
   }
